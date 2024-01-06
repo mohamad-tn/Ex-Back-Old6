501aeb23b63428ee279b2e63fc98eddb25f67e79
abdk123 <abdok1234@gmail.com>
2024-01-06 14:49:34 +0300

    5343

---
+++ src/Bwr.Exchange.EntityFrameworkCore/EntityFrameworkCore/Seed/Tenants/TenantDefaultTreasuryCreator.cs
@@ -0,0 +1,36 @@
+﻿using Bwr.Exchange.Settings.Treasuries;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Bwr.Exchange.EntityFrameworkCore.Seed.Tenants
+{
+    public class TenantDefaultTreasuryCreator
+    {
+        private readonly ExchangeDbContext _context;
+        private readonly int _tenantId;
+
+        public TenantDefaultTreasuryCreator(ExchangeDbContext context, int tenantId)
+        {
+            _context = context;
+            _tenantId = tenantId;
+        }
+
+        public void Create()
+        {
+            CreateTreasury();
+        }
+
+        private void CreateTreasury()
+        {
+            var treasury = _context.Treasuries.IgnoreQueryFilters().FirstOrDefault(r => r.Id == _tenantId && e.Name.Trim() == "الصندوق الرئيسي");
+            if (treasury == null)
+            {
+                treasury = new Treasury { Name = "الصندوق الرئيسي" };
+                treasury.TenantId = _tenantId;
+                _context.Treasuries.Add(treasury);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
